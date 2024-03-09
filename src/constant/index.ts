cedbba204f35ba7f779f7cf6984ae44a5086c497
mvc_ydb_admin <1609059419@qq.com>
2024-03-09 21:58:14 +0800

    feat: 🚀 add react backend app download

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,32 +1,32 @@
-import os = require('os');
+import os = require("os");
 
 export const USER_HOME_PATH = os.homedir();
-export const CLI_NAME = 'render-cli';
+export const CLI_NAME = "render-cli";
 
 export const APP_LIST: Array<{ name: string; value: string }> = [
   {
-    name: 'Component',
-    value: '@x.render/react-component-template',
+    name: "React Component",
+    value: "@x.render/react-component-template",
   },
   {
-    name: '模块库模版（module）',
-    value: '@render-ae86/ae86-module-template',
+    name: "React backend applications",
+    value: "@x.render/react-admin-template",
   },
   {
-    name: 'react中后台项目模板（web）',
-    value: '@render-ae86/ae86-admin-template',
+    name: "模块库模版（module）",
+    value: "@render-ae86/ae86-module-template",
   },
   {
-    name: 'react移动端H5应用模板 (h5)',
-    value: 'moga-h5',
+    name: "react移动端H5应用模板 (h5)",
+    value: "moga-h5",
   },
   {
-    name: 'react多页面应用模板（mpa）',
-    value: 'moga-mpa',
+    name: "react多页面应用模板（mpa）",
+    value: "moga-mpa",
   },
 ];
 
 export const BUILDER_CONFIG_FILE_TYPE = [
-  'build.json',
-  'build.config.(js|ts|mjs|mts|cjs|cts)',
+  "build.json",
+  "build.config.(js|ts|mjs|mts|cjs|cts)",
 ];
